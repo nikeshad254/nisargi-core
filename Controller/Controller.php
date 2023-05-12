8472ee6ca1834afc8260aefc03601a6addb7d383
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-05-12 09:38:23 +0545

    initializing mvc structure and files for nisargi

---
+++ Controller/Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+date_default_timezone_set('Asia/Kolkata');
+require_once('Model/Model.php');
+session_start();
+
+class Controller extends Model
+{
+
+	function __construct()
+	{
+		parent::__construct();
+
+		if (isset($_SERVER['PATH_INFO'])) {
+			switch ($_SERVER['PATH_INFO']) {
+				case '/':
+					include 'Views/index.php';
+					break;
+			}
+		}
+	}
+}
+
+$obj = new Controller;
---
+++ Model/Model.php
@@ -0,0 +1,24 @@
+<?php
+
+class Model {
+
+	protected $connection = '';
+	protected $servername = 'localhost';
+	protected $username = 'root';
+	protected $password = '';
+	protected $db = 'nisargi';
+
+	function __construct(){
+		
+		mysqli_report(MYSQLI_REPORT_STRICT);
+		try{
+			$this->connection = new mysqli($this->servername, $this->username, $this->password, $this->db);
+		} catch (Exception $ex) {
+			echo "Connection Failed: " . $ex->getMessage();
+			exit;
+		}
+	}
+
+}
+
+?>

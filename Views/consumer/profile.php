7776c0f8aba0ef42b792c5a3e7a04c174504d45b
Nikesh Adhikari <110375186+ultimatum254@users.noreply.github.com>
2023-07-17 08:13:04 +0545

    upload image implemented in new way

--- Views/consumer/profile.php
+++ Views/consumer/profile.php
@@ -3,23 +3,28 @@
     <form action="" method="post" enctype="multipart/form-data" class="profile-update-form">
         <div class="inp-label">
             <label for="name">Name</label>
-            <input type="text" name="name" value="<?=$userData['name']?>">
+            <input type="text" name="name" value="<?= $userData['name'] ?>">
         </div>
 
         <div class="inp-label">
             <label for="email">email</label>
-            <input type="text" name="email" value="<?=$userData['email']?>">
+            <input type="text" name="email" value="<?= $userData['email'] ?>">
         </div>
 
         <div class="inp-label">
             <label for="address">address</label>
-            <input type="text" name="address" value="<?=$userData['address']?>">
+            <input type="text" name="address" value="<?= $userData['address'] ?>">
         </div>
 
-        <div class="inp-label">
-            <label for="photo">photo</label>
-            <input type="file" name="photo">
+        <div class="photo-container">
+
+            <label class="label">
+                <img class="inputImg" src="uploads/<?= $userData['photo'] ?>">
+                <input type="file" name="photo" id="file" required />
+                <span>click to replace</span>
+            </label>
         </div>
+
 
         <div class="inp-label">
             <span>Change Password ?</span>
@@ -27,7 +32,17 @@
         </div>
 
         <button class="btn" type="submit">Update Profile</button>
-
-        <img src="uploads/<?=$userData['photo']?>" alt="" class="profileImg">
     </form>
 </div>
+
+<script>
+    let fileBtn = document.querySelector('#file');
+    let inputImg = document.querySelector('.inputImg');
+    fileBtn.addEventListener('change', (e) => {
+        let file = e.target.files[0];
+        console.log(e.target.files);
+        console.log(URL.createObjectURL(file));
+        console.log(file.name);
+        inputImg.src = URL.createObjectURL(file);
+    })
+</script>
